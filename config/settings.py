cbd92f74d07acffc7d865f98841049bfe809cf7c
Abdumalik Abdusalomov <aabdusalomov533@gmail.com>
2024-04-11 16:49:46 +0500

    contact branch

--- config/settings.py
+++ config/settings.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import io
+import os
 from pathlib import Path
-import dj_database_url
+from urllib.parse import urlparse
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zd$$f_njev7r7lk^iofwi61he%vop%u&m2i!as7)1zzu)8pnjm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -76,12 +76,45 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# [START db_setup]
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'HOST': '/cloudsql/qorgon:us-central1:qorgon',
+            'USER': 'qorgon',
+            'PASSWORD': 'Abdumalik0201912',
+            'NAME': 'qorgon',
+        }
     }
-}
+else:
+    # Running locally so connect to either a local MySQL instance or connect
+    # to Cloud SQL via the proxy.  To start the proxy via command line:
+    #    $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+            'NAME': 'qorgon',
+            'USER': 'qorgon',
+            'PASSWORD': 'Abdumalik0201912',
+        }
+    }
+# [END db_setup]
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'HOST': '127.0.0.1',
+#         'USER': 'qorgon',
+#         'PASSWORD': 'Abdumalik0201912',
+#         'NAME': 'qorgon',
+#     }
+# }
 
 
 # Password validation
@@ -119,10 +152,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
-MEDIA_URL = "/media/"
-MEDIA_ROOT = str(BASE_DIR.joinpath("media"))
+# STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
+STATIC_ROOT = 'static'
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
+from google.oauth2 import service_account
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    os.path.join(BASE_DIR, "credential.json")
+)
+
+DEFAULT_FILE_STORAGE = 'config.gcloud.GoogleCloudMediaFileStorage'
+GS_PROJECT_ID = 'qorgon'
+GS_BUCKET_NAME = 'qorgon.appspot.com'
+MEDIA_ROOT = "media/"
+UPLOAD_ROOT = 'media/photos/'
+MEDIA_URL = 'https://storage.googleapis.com/{}/'.format(GS_BUCKET_NAME)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
